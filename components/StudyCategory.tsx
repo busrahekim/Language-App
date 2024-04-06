256e905c8045cf0c70d08aed0cc853c1489779a1
bush <busra_hekim17@outlook.com>
2024-04-06 23:27:53 +0300

    collection card

--- components/StudyCategory.tsx
+++ components/StudyCategory.tsx
@@ -2,13 +2,23 @@ import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import StudyCategoryCard from "./StudyCategoryCard";
 
-const items = ["Daily Talk", "Find Location", "Greeting"];
+const items = [
+  { title: "Mistakes", img: require("@/assets/images/Zari_video_games.gif") },
+  { title: "Words", img: require("@/assets/images/Zari_lollipop.gif") },
+  { title: "New Learnings", img: require("@/assets/images/Zari_karate.gif") },
+];
 
 const StudyCategory = () => {
   return (
     <View className="flex my-3 ml-4">
-      <Text className="my-3 font-semibold text-slate-500 text-lg">Category Header</Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false} className="flex flex-row gap-2">
+      <Text className="my-3 font-semibold text-gray-800 text-lg">
+        My Collection
+      </Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="flex flex-row gap-2"
+      >
         {items.map((item, index) => (
           <StudyCategoryCard item={item} key={index} />
         ))}
--- components/StudyCategoryCard.tsx
+++ components/StudyCategoryCard.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { Entypo } from "@expo/vector-icons";
 
@@ -9,10 +9,15 @@ interface IProps {
 const StudyCategoryCard = ({ item }: IProps) => {
   return (
     <View className="rounded-md bg-white h-24 w-36 p-2 relative ml-2">
-      <Text className="mt-1 text-lg">{item}</Text>
+      <Text className="mt-1 text-lg">{item.title}</Text>
       <View className="h-4 w-4 bg-slate-100 rounded-md absolute bottom-0 right-0 m-3">
         <Entypo name="check" size={17} color="black" />
       </View>
+      <Image
+        source={item.img}
+        className="bg-transparent w-20 h-16"
+        style={{ resizeMode: "contain" }}
+      />
     </View>
   );
 };
